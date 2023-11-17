2c2166ccf6cb0dd0dae3be04b125d3cda2c46f6e
Douglas Fabiano <contatodouglasfabiano1@gmail.com>
2023-11-17 15:37:14 -0300

    PUT Product

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -31,9 +31,15 @@ public class ProductController {
 
     @PostMapping
     public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
-       dto = service.insert(dto);
-       URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-               .buildAndExpand(dto.getId()).toUri();
-       return ResponseEntity.created(uri).body(dto);
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ProductDTO> findById(@PathVariable Long id, @RequestBody ProductDTO dto) {
+        dto = service.update(id, dto);
+        return ResponseEntity.ok(dto);
     }
 }
